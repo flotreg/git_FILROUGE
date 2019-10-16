278145499c0d06fea16c3a08378ad70e0dbd5505
flotreg <ftreguilly@gmail.com>
2019-10-16 10:50:50 +0200

    Sur Out : -ajout de la fonction fill -ajout des attributs myPlace et myTransition -modification du toString -début du test de fill, à terminer

--- Projet_MAPD_FR/src/petriNetwork/Out.java
+++ Projet_MAPD_FR/src/petriNetwork/Out.java
@@ -2,6 +2,7 @@
  * 
  */
 package petriNetwork;
+import java.util.ArrayList;
 
 /**
  * Inherits abstract class Edge
@@ -10,6 +11,11 @@ package petriNetwork;
  */
 public class Out extends Edge {
 	
+	/*
+	 * ATTRIBUTES
+	 */
+	private Transition myTransition;
+	private Place myPlace;
 	/**
 	 * Constructor without parameters referring to Edge constructor
 	 */
@@ -24,9 +30,39 @@ public class Out extends Edge {
 	public Out(int weight) {
 		super(weight);
 	}
+	/*
+	 * GETTORS AND SETTORS
+	 */
 	
+	public Transition getMyTransition() {
+		return myTransition;
+	}
+
+	public void setMyTransition(Transition myTransition) {
+		this.myTransition = myTransition;
+	}
+
+	public Place getMyPlace() {
+		return myPlace;
+	}
+
+	public void setMyPlace(Place myPlace) {
+		this.myPlace = myPlace;
+	}
+	/*
+	 * Redefinition of toString Method
+	 * @see petriNetwork.Edge#toString()
+	 */
+
 	public String toString() {
-		return "I'm an edge Out and my weight is : "+this.getWeight();
+		return "In :\n"
+	+"Mon poids : "+this.getWeight()
+	+"\nMa place : "+this.myTransition
+	+"\nMa transition :"+this.myPlace;
+	}
+	
+	public void fill() {
+		myPlace.setTokens(myPlace.getTokens()+this.getWeight());
 	}
 
 	/**
@@ -35,13 +71,23 @@ public class Out extends Edge {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		
-		// TEST : constructor without parameters
+		// TEST1 : constructor without parameters
 		Edge eOut1 = new Out();
 		System.out.println(eOut1.toString());
 		
-		// TEST : constructor with parameters
+		// TEST2 : constructor with parameters
 		Edge eOut2 = new Out(7);
 		System.out.println(eOut2.toString());
+		
+		//TEST3 : fill() method
+		Out eOut3 = new Out(3);
+		ArrayList<Out> myOut = new ArrayList<Out>();
+		myOut.add(eOut3);
+		Place p = new Place(3,new ArrayList<In>(),myOut);
+		System.out.println(p);
+		eOut3.fill();
+		System.out.println(p);
+		
 
 	}
 
