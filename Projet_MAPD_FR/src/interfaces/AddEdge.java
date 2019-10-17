19035e8a24d4a42169e673bad7b681475c192ff6
fguibourt <44946638+fguibourt@users.noreply.github.com>
2019-10-17 10:07:05 +0200

    Ajout des descriptions au package interfaces

--- Projet_MAPD_FR/src/interfaces/AddEdge.java
+++ Projet_MAPD_FR/src/interfaces/AddEdge.java
@@ -1,25 +1,22 @@
-/**
- * 
- */
 package interfaces;
 
 import exceptions.AddEdgeException;
 
 /**
- * This interface permit to add edges
-<<<<<<< HEAD
+ * This interface makes it possible to add edges. 
  * @author Bonjour
-=======
- * @author Bonjour & François & Florian
->>>>>>> branch 'master' of https://github.com/flotreg/fil_rouge.git
+ * @version 1
+ * @since 17/10/2019
  *
  */
 public interface AddEdge {
+	
 	/**
-	 * This method add an edges between a this and a destination
-	 * @param dest
-	 * @param inOut
-	 * @param weight
+	 * This method adds an edge between the caller (this) and a destination (dest)
+	 * @param dest : the destination of the new edge
+	 * @param inOut (boolean) : type of the edge, In if true, Out if false 
+	 * @param weight : the weight of the edge
+	 * @throws AddEdgeException
 	 */
 	public void addEdge(AddEdge dest, boolean inOut, int weight) throws AddEdgeException;
 
--- Projet_MAPD_FR/src/interfaces/package-info.java
+++ Projet_MAPD_FR/src/interfaces/package-info.java
@@ -1,8 +1,10 @@
+
 /**
  * 
- */
-/**
+ * This packages contains all the interfaces required for this project
  * @author Bonjour
- *
+ * @version 1
+ * @since 17/10/2019
+ * 
  */
 package interfaces;
