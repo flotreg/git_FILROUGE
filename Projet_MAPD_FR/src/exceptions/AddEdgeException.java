a1ccabf9a68f36ea2df325079fbd7c095c46e5f0
fguibourt <44946638+fguibourt@users.noreply.github.com>
2019-10-17 10:00:45 +0200

    Ajout des descriptions pour les classes du package exceptions

--- Projet_MAPD_FR/src/exceptions/AddEdgeException.java
+++ Projet_MAPD_FR/src/exceptions/AddEdgeException.java
@@ -4,13 +4,16 @@
 package exceptions;
 
 /**
+ * Exception to throw when building edges
  * @author Bonjour
- *
+ * @version 1
+ * @since 17/10/2019
+ * 
  */
 public class AddEdgeException extends Exception{
 
 	/**
-	 * default serial number
+	 *Default serial number automatically generated
 	 */
 	private static final long serialVersionUID = 1L;
 	
