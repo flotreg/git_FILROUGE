503ab3f317ee2a769e18818000cacf28ad10f1f4
fguibourt <44946638+fguibourt@users.noreply.github.com>
2019-11-26 18:24:41 +0100

    package info

--- mapd_pne_code_treguib/src/org/pneditor/petrinet/adapters/treguib/package-info.java
+++ mapd_pne_code_treguib/src/org/pneditor/petrinet/adapters/treguib/package-info.java
@@ -1,1 +1,12 @@
+/**
+ * This packages holds our Adapters to make the connection between our code and
+ * PNE code.
+ * Each Adapter inherits from the abstract object from PNE code. 
+ * Each Adapter has the corresponding object as attribute from our code.
+ * List of Adapters :
+ * - PetriNetAdapter
+ * - PlaceAdapter
+ * - TransitionAdapter
+ * - ArcAdapter
+ */
 package org.pneditor.petrinet.adapters.treguib;
--- mapd_pne_code_treguib/src/org/pneditor/petrinet/models/treguib/package-info.java
+++ mapd_pne_code_treguib/src/org/pneditor/petrinet/models/treguib/package-info.java
@@ -1,1 +1,5 @@
+/**
+ * This package holds our code. Classes in this package will be called through
+ * the Adapters in the adapters package.
+ */
 package org.pneditor.petrinet.models.treguib;
